58cf6343050d8b5211431f6889d3a3c9a5901621
Alejandro <alegonzalez21195@gmail.com>
2020-12-17 15:37:35 -0600

    you can create a new event, get event, register event by id and approval or rejection register

--- event/src/controllers/EventController.js
+++ event/src/controllers/EventController.js
@@ -1,8 +1,8 @@
-const events = require("../models/Event.js");
+const Event = require("../models/Event.js");
 const users = require("../models/User.js");
 module.exports = {
   async createEvent(req, res) {
-    const { title, description, price, thumbnail } = req.body;
+    const { title, description, price, thumbnail, sport } = req.body;
     const { user_id } = req.headers;
     const { filename } = req.file;
 
@@ -12,25 +12,26 @@ module.exports = {
       return res.status(400).json({ message: "User does not exist!" });
     }
 
-    const event = await events.create({
+    const event = await Event.create({
       title,
       description,
       price: parseFloat(price),
       user: user_id,
       thumbnail: filename,
+      sport,
     });
 
     return res.json(event);
   },
-  async getEventById(req, res) {
+  async deleteEvent(req, res) {
     const { eventId } = req.params;
     try {
-      const event = await events.findById(eventId);
-      if (event) {
-        return res.json(event);
-      }
+      await Event.findByIdAndDelete(eventId);
+      return res.status(204)
     } catch (error) {
-      return res.status(400).json({ message: "EventId does not exist!" });
+      return res
+        .status(404)
+        .json({ message: "We do no have any events with this id yet" });
     }
   },
 };
